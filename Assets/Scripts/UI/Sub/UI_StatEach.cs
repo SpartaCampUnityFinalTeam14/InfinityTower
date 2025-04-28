edad89e755ba561a1c5cc364c811a1960720e7ae
KSangMin <fhfpszmfnwm@naver.com>
2025-04-28 17:05:08 +0900

    [Feat] 타워 정보 출력 UI 동적 생성

---
+++ Assets/Scripts/UI/Sub/UI_StatEach.cs
@@ -0,0 +1,17 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_StatEach : MonoBehaviour
+{
+    [SerializeField] private Image statIcon;
+    [SerializeField] private TextMeshProUGUI statNameText;
+    [SerializeField] private TextMeshProUGUI statValueText;
+
+    public void Init(TowerStatType type, string name, float value)
+    {
+        //스프라이트 세팅
+        statNameText.text = name;
+        statValueText.text = value.ToString();
+    }
+}
