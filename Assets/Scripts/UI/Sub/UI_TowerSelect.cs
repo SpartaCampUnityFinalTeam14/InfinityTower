c69f51a1d4f467bdff1b25aee73cb3e021e4436a
KSangMin <fhfpszmfnwm@naver.com>
2025-05-08 12:51:56 +0900

    [Feat] 무한 스크롤 구현

--- Assets/Scripts/UI/Sub/UI_TowerSelect.cs
+++ Assets/Scripts/UI/Sub/UI_TowerSelect.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,8 +9,14 @@ public class UI_TowerSelect : MonoBehaviour
     [HideInInspector] public UI_Deck deck;
 
     [SerializeField] private GameObject scrollView;
-    [SerializeField] private Transform slotParent;
+    [SerializeField] private ScrollRect scrollRect;
+    [SerializeField] private RectTransform content;
     private List<UI_TowerSelectSlot> slots = new();
+
+    public int slotWidth = 270;
+    public int spacing = 20;
+    public int poolCount = 7;
+    private int prevIndex = -1;
 
     [SerializeField] private IntEventChannel OnTowerSlotSelected;
     [SerializeField] private IntEventChannel OnTowerLevelChanged;
@@ -17,6 +24,8 @@ public class UI_TowerSelect : MonoBehaviour
 
     private void Awake()
     {
+        scrollRect.onValueChanged.AddListener(UpdateSlots);
+
         UnregisterListeners();
         RegisterListeners();
 
@@ -37,33 +46,60 @@ public class UI_TowerSelect : MonoBehaviour
         OnTowerExpChanged.RegisterListener(UpdateTowerSlotExp);
     }
 
-    public void Init()
+    void Init()
     {
-        foreach (Transform child in slotParent) Destroy(child.gameObject);
+        foreach (Transform child in content) Destroy(child.gameObject);
         slots.Clear();
 
-        foreach(var data in DataManager.Instance.towerDict)
+        InitContentSize();
+
+        for (int i = 0; i < poolCount; i++)
         {
-            UI_TowerSelectSlot slot = Util.InstantiatePrefabAndGetComponent<UI_TowerSelectSlot>(path: "UI/Sub/UI_TowerSelectSlot", parent: slotParent);
+            UI_TowerSelectSlot slot = Util.InstantiatePrefabAndGetComponent<UI_TowerSelectSlot>(path: "UI/Sub/UI_TowerSelectSlot", parent: content);
             slots.Add(slot);
-
-            slot.Init(data.Value.id);
+            slot.gameObject.SetActive(false);
         }
 
-        foreach(int index in SaveManager.Instance.playerData.selectedTowerIndex)
-        {
-            foreach(var slot in slots)
-            {
-                if (slot.id == index) slot.SetSelectedMark(true);
-            }
-        }
+        UpdateSlots();
+    }
+
+    void InitContentSize()
+    {
+        int slotSize = slotWidth + spacing;
+        int contentSize = DataManager.Instance.towerDict.Count * slotSize;
+        content.sizeDelta = new Vector2(contentSize, content.sizeDelta.y);
+    }
+
+    void UpdateSlots(Vector2 scroll)
+    {
+        UpdateSlots();
     }
 
     public void UpdateSlots()
     {
-        foreach(var slot in slots)
+        float scrollX = content.anchoredPosition.x;
+        int slotSize = slotWidth + spacing;
+        int curIndex = (int)MathF.Floor(-scrollX / slotSize);
+
+        if (prevIndex == curIndex) return;
+        prevIndex = curIndex;
+        Debug.Log(curIndex);
+
+        for(int i = 0; i < poolCount; i++)
         {
-            slot.UpdateSlot();
+            int dataIndex = curIndex + i;
+            if(0 <= dataIndex && dataIndex < DataManager.Instance.towerDict.Count)
+            {
+                var slot = slots[i];
+                int dictKey = DataManager.Instance.towerDict.Keys.ToList()[dataIndex];
+                slot.Init(dictKey);
+
+                slot.gameObject.SetActive(true);
+                RectTransform slotRectTransform = slot.GetComponent<RectTransform>();
+                float slotX = dataIndex * slotSize;
+                slotRectTransform.anchoredPosition = new Vector2(slotX, 0);
+            }
+            else slots[i].gameObject.SetActive(false);
         }
     }
 
